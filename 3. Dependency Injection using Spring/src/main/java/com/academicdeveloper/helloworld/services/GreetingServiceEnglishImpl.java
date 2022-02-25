94d0b213bfa9ee4f303288930c637287529915f6
DRRONIDZ <abdelhalim.diab@univ-constantine2.dz>
2022-02-25 23:48:56 +0100

    3. Dependency Injection using Spring Using default profiles. closes #6.

--- 3. Dependency Injection using Spring/src/main/java/com/academicdeveloper/helloworld/services/GreetingServiceEnglishImpl.java
+++ 3. Dependency Injection using Spring/src/main/java/com/academicdeveloper/helloworld/services/GreetingServiceEnglishImpl.java
@@ -11,7 +11,7 @@ import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Component;
 
 @Component
-@Profile("english")
+@Profile({"default","english"})
 public class GreetingServiceEnglishImpl implements GreetingService {
 
     @Override
