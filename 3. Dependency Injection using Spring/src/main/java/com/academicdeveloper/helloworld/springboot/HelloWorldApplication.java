22ff12ec609c04bd3d6ac84a56dff355e1dc6dd2
DRRONIDZ <abdelhalim.diab@univ-constantine2.dz>
2022-02-26 11:34:01 +0100

    4. Spring Java Configuration using Spring XML Configuration with Spring boot. closes #11.

--- 3. Dependency Injection using Spring/src/main/java/com/academicdeveloper/helloworld/springboot/HelloWorldApplication.java
+++ 3. Dependency Injection using Spring/src/main/java/com/academicdeveloper/helloworld/springboot/HelloWorldApplication.java
@@ -5,12 +5,14 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.ImportResource;
 
 /*
 * Spring 4 & Spring Boot
 * */
 @SpringBootApplication
-@ComponentScan("com.academicdeveloper.helloworld")
+//@ComponentScan("com.academicdeveloper.helloworld")
+@ImportResource("classpath:/spring/spring-config.xml")
 public class HelloWorldApplication {
 
     public static void main(String[] args) {
